35af351ac83a4d7a324850ac02be166466028d55
tahungoding <tahungoding@gmail.com>
2021-08-03 20:48:22 +0700

    fucking title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>
-        SIPELAKON - Sistem Pelaporan dan
+        SIPELAKON
     </title>
     <meta name="description" content="Dinas, Koperasi, UMKM" />
     <meta name="keywords" content="SIPELAKON" />
