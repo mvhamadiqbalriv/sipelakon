757b8e655e109e92449eb994b4d9e6d8dcfb72b5
tahungoding <tahungoding@gmail.com>
2021-08-31 18:59:37 +0700

    change validation upload to 10mb

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -59,7 +59,7 @@ class PostController extends Controller
 
         $path = null;
         if ($request->file('file')) {
-            $validation['file'] = 'file|max:3000';
+            $validation['file'] = 'file|max:10000';
         }
         
         $request->validate($validation);
@@ -126,7 +126,7 @@ class PostController extends Controller
         ];
 
         if ($request->file('file')) {
-            $validation['file'] = 'file|max:3000';
+            $validation['file'] = 'file|max:10000';
         }
         
         $request->validate($validation);
@@ -175,7 +175,7 @@ class PostController extends Controller
 
         $path = null;
         if ($request->file('file')) {
-            $validation['file'] = 'file|max:3000';
+            $validation['file'] = 'file|max:10000';
         }
 
         $request->validate($validation);
